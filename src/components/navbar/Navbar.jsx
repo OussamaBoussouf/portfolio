f8d016e8de9c707e6fe0e642cf42dc33844b97bf
Oussama Bof <jackmarrow06@gmail.com>
2024-04-01 00:15:21 +0200

    Navbar section has been created

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,60 @@
-import './navbar.css';
+import { AlignRight, X } from "lucide-react";
+import { easeInOut, motion } from "framer-motion";
+import "./navbar.css";
+import { useState } from "react";
 
+const variants = {
+  open: { height: "auto" },
+  closed: { height: 0 },
+  //   test: { duration: 5, ease: "easeInOut" },
+};
 
+const links = ["Home", "About", "Projects", "Skills", "Contact"];
 
 function Navbar() {
-    return (
-        <div>
-            Navbar
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <nav className="navbar">
+      <div className="wrapper">
+        <div className="navbar__container">
+          <div className="navbar__logo">
+            <a href="#">Developo.</a>
+          </div>
+          <ul className="navbar__list">
+            {links.map((link) => (
+              <li className="navbar__item">
+                <a href="">{link}</a>
+              </li>
+            ))}
+          </ul>
+          <button
+            onClick={() => setIsOpen(!isOpen)}
+            className="navbar__btn"
+            type="button"
+          >
+            {isOpen ? (
+              <X color="white" size={30} />
+            ) : (
+              <AlignRight color="white" size={30} />
+            )}
+          </button>
+          <motion.div
+            animate={isOpen ? "open" : "closed"}
+            variants={variants}
+            className="navbar__menu"
+          >
+            <ul className="navbar__menu__list">
+            {links.map((link) => (
+              <li className="navbar__menu__item">
+                <a href="">{link}</a>
+              </li>
+            ))}
+            </ul>
+          </motion.div>
         </div>
-    );
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
